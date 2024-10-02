8e385ce09013eb4db1b5453984c5791dbbb78791
Luca <luca.cavalcanti@studenti.unitn.it>
2024-10-02 12:45:34 +0200

    Add converter file

---
+++ utils/convert.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import time
+import datetime
+
+def convert(filepath):
+    old_file = open(filepath, 'r')
+    new_file = open(filepath[:-4]+'.txt', "w")
+
+    lines = old_file.readlines()
+    old_file.close()
+
+    nodes = {}
+    node_counter = 0
+
+    lines = lines[1:]
+
+    for line in lines:
+        values = line.split()
+        src = values[0]
+        dst = values[1]
+
+        src_id, dst_id = 0, 0
+
+        timestamp = values[3] + " " + values[4]
+
+        if src in nodes.keys():
+            src_id = nodes[src]
+        else:
+            src_id = node_counter
+            node_counter += 1
+            nodes[src] = src_id
+
+        if dst in nodes.keys():
+            dst_id = nodes[dst]
+        else:
+            dst_id = node_counter
+            node_counter += 1
+            nodes[dst] = dst_id
+
+        d = datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+        unixtime = time.mktime(d.timetuple())
+
+        new_file.write(str(src_id) + " " + str(dst_id) + " " + str(int(unixtime)) + "\n")
+
+            
+
+convert("../../data/soc-redditHyperlinks-body.tsv")
