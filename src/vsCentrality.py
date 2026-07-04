96898d81c7f93d1dbe9b8ba76b8bc59303c5f477
LucaCavalcanti <101122897+LucaCavalcanti@users.noreply.github.com>
2026-07-04 16:19:38 +0200

    Refactor, Add debug mode,  Delete old classes

--- src/vsCentrality.py
+++ src/vsCentrality.py
@@ -1,135 +1,53 @@
-# ------------------------- class Node -------------------------
-
 from collections import defaultdict
 from operator import itemgetter
 from typing import Set, List, Dict
 from infectionSimulation import simulate_infection
 
 from settings import *
-from statistics import stats # type: ignore
-
-PROB_OF_BEING_INFECTED = 0.8
-
-# ------------------------- class Tree -------------------------
-
-class Forest:
-    # constructor
-    def __init__(self):
-        # initialize adjacency list
-        self.adjacency_list = defaultdict(dict)
-        
-    def add_edge(self, src, dst):
-        if src not in self.adjacency_list:
-            self.adjacency_list[src] = dict()
-        if dst not in self.adjacency_list:
-            self.adjacency_list[dst] = dict()
-            
-        self.adjacency_list[src][dst] = None
-        self.adjacency_list[dst][src] = None
-    
-    def add_node(self, node):
-        if node not in self.adjacency_list:
-            self.adjacency_list[node] = dict()
-    
-    def get_adjacency_list(self):
-        return {k: list(v.keys()) for k, v in self.adjacency_list.items()}
-
-
-# ------------------------- class Node -------------------------
-
-class Node:
-    
-    def __init__(self, id, timestamp):
-        ''''
-        init function of the class Node
-        '''
-        self.id = id
-        self.timestamp = timestamp
-        self.children = []
-        self.subtree_size = 0
-
-    def add_child(self, child):
-        '''
-        add a child to the node
-        '''
-        self.children.append(child)
-
-    def __repr__(self):
-        return f"{self.id}, {self.timestamp}"
-    
-# ------------------------ print tree ------------------------
-
-def print_tree(tree, spaces=0):
-    ''''
-    function that print the tree as an horizontal tree
-    if spaces are passed in input, the tree will be printed with the number of spaces passed in input
-    '''
-    print(" " * spaces, tree)
-    for child in tree.children:
-        print_tree(child, spaces+1)
 
 # ------------------------- choose nodes --------------------------------
 
 def find_best_node (nodes : Dict[int, int], budget : int, seed_set: Set[int]) -> List[int]:
-
     # sort the nodes by their subtree size
     sorted_nodes = {k: v for k, v in sorted(nodes.items(), key=itemgetter(1), reverse=True) if k not in seed_set}
     return list(sorted_nodes.keys())[:budget]
 
-# to choose the nodes with the centrality algorithm, we'll use the find_best_node function
-
 # ------------------------- Main -------------------------
 
-def centrality_analysis(filename: str, seed_set: set, node_budget: int, selected_nodes_subtree: set, prob: float = PROB_OF_BEING_INFECTED):
-
-    # dictionary that contains the number of times that each node that compare in the subtree algorithm
-    #removed_nodes_subtree = defaultdict(int)
-
-    # dictionary that contains the number of times that compare in the centrality algorithm
-    nodes_centrality = defaultdict(int)
-
-    first_simulation = simulate_infection (seed_set, filename, prob, nodes=nodes_centrality)
-
-    """ # simulation and selection of the nodes with the subtree algorithm
-    for _ in range (times):
-        forest = forward_forest (seed_set, filename, prob)
-
-        selected_node_subtree = choose_nodes (forest, seed_set, node_budget)
-        for node in selected_node_subtree:
-            removed_nodes_subtree[node] = removed_nodes_subtree[node] + 1
-
-    selected_nodes_subtree = find_best_node (removed_nodes_subtree, node_budget)
-    print(f"Selected nodes subtree: {selected_nodes_subtree}") """
-
-    # print(f"Selected nodes (Subtree method): {selected_nodes_subtree}")
-
-    average_subtree = 0
-    for _ in range(times):
-        second_simulation_subtree = simulate_infection (seed_set, filename, prob, removed_nodes=selected_nodes_subtree)
-        average_subtree += len(second_simulation_subtree)
-    # print(f"Average number of infected nodes, subtree method: {average_subtree/times}")
-
+def centrality_analysis(filename: str, seed_set: set, attackset_budget: int, prob: float, nodes_centrality: Dict[int, int] = None):
+    '''
+    function that finds an attack set using the centrality method and simulates the infection with the attack set
+    
+    input:
+        - filename: string, the name of the file containing the information about the network
+        - seed_set: set, set of nodes selected to maximize the spread of the influence
+        - attackset_budget: int, the maximum size of the attack set
+        - prob: float, probability of a node of being infected
+        - nodes_centrality: dict, dictionary containing the nodes and their centrality measure
+        
+    output:
+        - total_infected_list_centrality: list, list of the number of total infected nodes for each simulation
+    '''
     # simulation and selection of the nodes with the centrality algorithm
-    selected_nodes_centrality = find_best_node (nodes_centrality, node_budget, seed_set)
-    # print(f"Selected nodes (Centrality method): {selected_nodes_centrality}")
+    selected_nodes_centrality = find_best_node (nodes_centrality, attackset_budget, seed_set)
+    if(DEBUG): print(f"Attack set (Centrality method): {selected_nodes_centrality}")
 
-    stats.simulation_type = "centrality"
     average_centrality = 0
-    for _ in range(times):
+    total_infected_list_centrality = []
+    for _ in range(times_infection):
         second_simulation_centrality = simulate_infection (seed_set, filename, prob, removed_nodes=selected_nodes_centrality)
         average_centrality += len(second_simulation_centrality)
-    # print(f"Average number of infected nodes, centrality method: {average_centrality/times}")
-    stats.simulation_type = "none"
+        total_infected_list_centrality.append(len(second_simulation_centrality))
 
-    mean_infected = average_centrality / times
+    if(DEBUG): print(f"list of total infected nodes, centrality method: {total_infected_list_centrality}")
     
-    return selected_nodes_centrality, mean_infected
+    return total_infected_list_centrality
 
 if __name__ == "__main__":
     
     filename = "data/email.txt"
     seed_set = {83, 49, 60, 85}
-    node_budget = 10
+    attackset_budget = 10
     subtree = set()
     
-    centrality_analysis(filename, seed_set, node_budget, subtree)
+    centrality_analysis(filename, seed_set, attackset_budget, subtree)
--- src/vsRandom.py
+++ src/vsRandom.py
@@ -1,53 +1,16 @@
-# ------------------------- class Node -------------------------
-
 import random
 from typing import Set
 from infectionSimulation import simulate_infection
-from subTreeInfection import subtrees_methods
 
 from settings import *
-from statistics import stats # type: ignore
-
-PROB_OF_BEING_INFECTED = 0.2
-
-class Node:
-    
-    def __init__(self, id, timestamp):
-        ''''
-        init function of the class Node
-        '''
-        self.id = id
-        self.timestamp = timestamp
-        self.children = []
-        self.subtree_size = 0
-
-    def add_child(self, child):
-        '''
-        add a child to the node
-        '''
-        self.children.append(child)
-
-    def __repr__(self):
-        return f"{self.id}, {self.timestamp}"
-    
-# ------------------------ print tree ------------------------
-
-def print_tree(tree, spaces=0):
-    ''''
-    function that print the tree as an horizontal tree
-    if spaces are passed in input, the tree will be printed with the number of spaces passed in input
-    '''
-    print(" " * spaces, tree)
-    for child in tree.children:
-        print_tree(child, spaces+1)
-
+from statistics import stats
 
 # ------------------------- Random Algorithm -------------------------
 
 def choose_random_nodes (budget : int, seed_set: Set[int], nodes: Set[int]):
     '''
-    function that choose k nodes randomly
-    input: budget is the number of nodes to choose
+    function that chooses 'budget' nodes randomly
+    input: budget is the number of nodes to choose, seed_set is the set of nodes selected to maximize the spread of the influence, nodes is the set of all nodes in the graph
     output: the set of nodes chosen
     '''
     set_chosen_nodes = set()
@@ -58,41 +21,22 @@ def choose_random_nodes (budget : int, seed_set: Set[int], nodes: Set[int]):
         set_chosen_nodes.add(chosen_node)
     return set_chosen_nodes
 
-def random_analysis(filename: str, seed_set: set, node_budget: int, prob: float = PROB_OF_BEING_INFECTED, selected_nodes_subtree = []):
-
-    # set that contains all the nodes of the graph
-    nodes = set()
-
-
-    first_simulation = simulate_infection (seed_set, filename, prob, nodes_random=nodes)
-    # print(f"First simulation: {len(first_simulation)}")
-
-    if selected_nodes_subtree == []:
-        selected_nodes_subtree = subtrees_methods(filename, set(seed_set), node_budget, prob)
-   
-    # print(f"Selected nodes (Subtree method): {selected_nodes_subtree}")
-
-    average_subtree = 0
-    for _ in range(times):
-        second_simulation_subtree = simulate_infection (seed_set, filename, prob, removed_nodes=selected_nodes_subtree)
-        average_subtree += len(second_simulation_subtree)
-    # print(f"Average number of infected nodes, subtree method: {average_subtree/times}")
-
+def random_analysis(filename: str, seed_set: set, node_budget: int, prob: float, nodes_random: Set[int] = None):
     # simulation and selection of the nodes with the random algorithm
-    selected_node_random = choose_random_nodes (node_budget, seed_set, nodes)
-    # print(f"Selected nodes (Random method): {selected_node_random}")
+    selected_node_random = choose_random_nodes (node_budget, seed_set, nodes_random)
+    if(DEBUG): print(f"Attack set (Random method): {selected_node_random}")
     
     stats.simulation_type = "random"
     average_random = 0
-    for _ in range(times):
+    total_infected_list_random = []
+    for _ in range(times_infection):
         second_simulation_random = simulate_infection (seed_set, filename, prob, removed_nodes=selected_node_random)
         average_random += len(second_simulation_random)
-    # print(f"Average number of infected nodes, random method: {average_random/times}")
-    stats.simulation_type = "none"
+        total_infected_list_random.append(len(second_simulation_random))
 
-    mean_infected = average_random / times
+    if(DEBUG): print(f"list of total infected nodes, random method: {total_infected_list_random}")
     
-    return selected_node_random, mean_infected
+    return total_infected_list_random
 
 # ------------------------- Main -------------------------
 
@@ -101,4 +45,5 @@ if __name__ == "__main__":
     filename = "data/email.txt"
     seed_set = {83, 49, 60, 85}
     node_budget = 20
-    random_analysis(filename, seed_set, node_budget)
+    prob = 0.2
+    random_analysis(filename, seed_set, node_budget, prob)
